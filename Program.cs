c03d62f99f69f4f7e196179835e35ddf38b8b089
saulkaique <e107ti@etec.sp.gov.br>
2023-08-15 18:31:41 -0300

    Eufiz algumas coisas

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+//declarando as variaveis de texto
+string tex1 = "" , tex2 = "";
+//declarando as variaveis inteiras 
+int numcar = 0 , num = 0 , escolha ;
+
+string p1 = @"Bom dia e para deixar claro sempre é bom dia então vamos começar 
+seja bem-vindo nesse projeto você vai poder digitar uma frase e ela será criptografada agora vamos 
+aperte qualquer botão"; 
+  
+  
+  for (int i1 = 0; i1 < f1.Length; i1++)
+        {
+            Console.Write(f1.ElementAt(i1));
+            Thread.Sleep(60);
+
+        }
+
+
+    
+   
+        Console.WriteLine("Digite o texto que deseja Criptografar!");
+        tex1 = Console.ReadLine()!;
+        numcar = tex1.Length;
+        
+        
+        
+        for (int i = 0; i < length; i++)
+        {
+            
+        }
+
